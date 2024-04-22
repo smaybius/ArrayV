f4b990cd038cb02894b36fa39ec9dfabf8704755
Connor Mitchell <37678351+smaybius@users.noreply.github.com>
2024-04-22 03:05:14 -0400

    cleanup personal groovys

--- DistribTest.groovy
+++
@@ -1,8 +0,0 @@
-import io.github.arrayv.prompts.SortPrompt
-import io.github.arrayv.groovyapi.GroovyLocals
-//Warning: With this current code, this script will run as soon as ArrayV is loaded if it's in the scripts/ folder.
-runGroup(256) { //made to test non-deterministic shuffles that might throw exceptions
-        for (int i in 0..256) {
-            run OptimizedPigeonholeSort go 128.numbers, 2048.speed
-        }
-    }
--- src/main/java/io/github/arrayv/sortdata/SortMeta.java
+++ src/main/java/io/github/arrayv/sortdata/SortMeta.java
@@ -16,7 +16,7 @@ public @interface SortMeta {
      * {@code "Something Sort"}. If not
      * specified, all three of {@link #listName()}, {@link #runName()}, and
      * {@link #showcaseName()} must be specified.
-     * 
+     *
      * @return The sort's annotated name.
      */
     String name() default "";
@@ -25,7 +25,7 @@ public @interface SortMeta {
      * The sort's category. This may be specified on a package-level in
      * package-info.java. Specifying it here will
      * override it for the package.
-     * 
+     *
      * @return The sort's category, or {@code ""} if you should look at the package
      *         level.
      */
@@ -33,14 +33,14 @@ public @interface SortMeta {
 
     /**
      * Explicit sort list name. This is generated from {@link #name()} by default.
-     * 
+     *
      * @return The sort's explicit list name, if it has one. {@code ""} otherwise.
      */
     String listName() default "";
 
     /**
      * Explicit Run Sort name. This is generated from {@link #name()} by default.
-     * 
+     *
      * @return The sort's explicit Run Sort name, if it has one. {@code ""}
      *         otherwise.
      */
@@ -49,7 +49,7 @@ public @interface SortMeta {
     /**
      * Explicit Showcase Sorts name (and scripting name). This is generated from
      * {@link #name()} by default.
-     * 
+     *
      * @return The sort's explicit Showcase Sorts name, if it has one. {@code ""}
      *         otherwise.
      */
@@ -57,7 +57,7 @@ public @interface SortMeta {
 
     /**
      * Whether this sort is disabled. Disabled sorts won't be loaded.
-     * 
+     *
      * @return Whether this sort is disabled.
      */
     boolean disabled() default false;
@@ -65,14 +65,14 @@ public @interface SortMeta {
     /**
      * This sort's unreasonable limit. If the sort is run with lengths higher than
      * this, a warning is displayed.
-     * 
+     *
      * @return This sort's unreasonable limit.
      */
     int unreasonableLimit() default 0;
 
     /**
      * Whether to treat this sort as slow in Showcase Sorts and in sort scripts.
-     * 
+     *
      * @return Whether to treat this sort as slow.
      */
     boolean slowSort() default false;
@@ -80,21 +80,21 @@ public @interface SortMeta {
     /**
      * Whether this sort is a bogo sort (i.e. it has "bogo" in its name, and it's
      * non-deterministic).
-     * 
+     *
      * @return Whether this sort is a bogo sort.
      */
     boolean bogoSort() default false;
 
     /**
      * Whether this sort is a Radix Sort.
-     * 
+     *
      * @return Whether this sort is a Radix Sort.
      */
     boolean radixSort() default false;
 
     /**
      * Whether this sort uses buckets.
-     * 
+     *
      * @return Whether this sort uses buckets.
      */
     boolean bucketSort() default false;
@@ -103,7 +103,7 @@ public @interface SortMeta {
      * A question to ask the user when they choose this sort. You can perform
      * response validation by creating a method
      * that is {@code public static int validateAnswer(int answer)}.
-     * 
+     *
      * @return The question to ask the user, or {@code ""} if there isn't one.
      */
     String question() default "";
@@ -113,7 +113,7 @@ public @interface SortMeta {
      * user pressed "Use default". This
      * value is ignored if there is no question. This value is <i>not</i> passed
      * through {@code validatorAnswer}.
-     * 
+     *
      * @return The default answer response.
      */
     int defaultAnswer() default 0;
